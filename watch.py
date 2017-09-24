6ed9e61085b93fb9ab1ec4249c1feb474847d4ce
James Bowman <jamesb@excamera.com>
2017-09-23 19:38:54 -0700

    Watch testbench

---
+++ watch.py
@@ -0,0 +1,124 @@
+import numpy as np
+import time
+import Image
+
+class vec3():
+    def __init__(self, x, y, z):
+        (self.x, self.y, self.z) = (x, y, z)
+    def __mul__(self, other):
+        return vec3(self.x * other, self.y * other, self.z * other)
+    def __add__(self, other):
+        return vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+    def __sub__(self, other):
+        return vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+    def dot(self, other):
+        return (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
+    def __abs__(self):
+        return self.dot(self)
+    def norm(self):
+        mag = np.sqrt(abs(self))
+        return self * (1.0 / np.where(mag == 0, 1, mag))
+    def components(self):
+        return (self.x, self.y, self.z)
+rgb = vec3
+
+(w, h) = (128, 128)         # Screen size
+L = vec3(5, 5., -10)        # Point light position
+E = vec3(0., 0, 0)     # Eye position
+FARAWAY = 1.0e39            # an implausibly huge distance
+gen = 0
+
+def raytrace(O, D, scene, bounce = 0):
+    # O is the ray origin, D is the normalized ray direction
+    # scene is a list of Sphere objects (see below)
+    # bounce is the number of the bounce, starting at zero for camera rays
+
+    distances = [s.intersect(O, D) for s in scene]
+    nearest = reduce(np.minimum, distances)
+    color = rgb(0, 0, 0)
+    for (s, d) in zip(scene, distances):
+        cc = s.light(O, D, d, scene, bounce) * (nearest != FARAWAY) * (d == nearest)
+        color += cc
+        _rgb = [Image.fromarray((255 * np.clip(c, 0, 1).reshape((h, w))).astype(np.uint8), "L") for c in cc.components()]
+        global gen
+        Image.merge("RGB", _rgb).save("p%02d.png" % gen)
+        gen += 1
+    return color
+
+class Sphere:
+    def __init__(self, center, r, diffuse, mirror = 0.5):
+        self.c = center
+        self.r = r
+        self.diffuse = diffuse
+        self.mirror = mirror
+
+    def intersect(self, O, D):
+        b = 2 * D.dot(O - self.c)
+        c = abs(self.c) + abs(O) - 2 * self.c.dot(O) - (self.r * self.r)
+        disc = (b ** 2) - (4 * c)
+        sq = np.sqrt(np.maximum(0, disc))
+        h0 = (-b - sq) / 2
+        h1 = (-b + sq) / 2
+        h = np.where((h0 > 0) & (h0 < h1), h0, h1)
+
+        pred = (disc > 0) & (h > 0)
+        return np.where(pred, h, FARAWAY)
+
+    def diffusecolor(self, M):
+        return self.diffuse
+
+    def light(self, O, D, d, scene, bounce):
+        return self.diffuse
+        M = (O + D * d)                         # intersection point
+        N = (M - self.c) * (1. / self.r)        # normal
+        toL = (L - M).norm()                    # direction to light
+        toO = (E - M).norm()                    # direction to ray origin
+        nudged = M + N * .0001                  # M nudged to avoid itself
+
+        # Shadow: find if the point is shadowed or not.
+        light_distances = [s.intersect(nudged, toL) for s in scene]
+        light_nearest = reduce(np.minimum, light_distances)
+        seelight = light_distances[scene.index(self)] == light_nearest
+        seelight = 1
+
+        # Ambient
+        color = rgb(0.05, 0.05, 0.05)
+
+        # Lambert shading (diffuse)
+        lv = np.maximum(N.dot(toL), 0)
+        color += self.diffusecolor(M) * lv * seelight
+
+        # Reflection
+        if 0: # bounce < 2:
+            rayD = (D - N * 2 * D.dot(N)).norm()
+            color += raytrace(nudged, rayD, scene, bounce + 1) * self.mirror
+
+        # Blinn-Phong shading (specular)
+        phong = N.dot((toL + toO).norm())
+        color += rgb(1, 1, 1) * np.power(np.clip(phong, 0, 1), 50) * seelight
+        return color
+
+class CheckeredSphere(Sphere):
+    def diffusecolor(self, M):
+        checker = ((M.x * 2).astype(int) % 2) == ((M.z * 2).astype(int) % 2)
+        return self.diffuse * checker
+
+scene = [
+    Sphere(vec3(0, 0, 2), .6, rgb(0, 0, 1)),
+    # Sphere(vec3(-1, -1, 2), .6, rgb(0, 1, 0)),
+    # Sphere(vec3(1, 1, 2), .6, rgb(1, 0, 0)),
+    ]
+
+r = float(w) / h
+# Screen coordinates: x0, y0, x1, y1.
+S = (-1., 1., 1., -1.)
+x = np.tile(np.linspace(S[0], S[2], w), h)
+y = np.repeat(np.linspace(S[1], S[3], h), w)
+
+t0 = time.time()
+Q = vec3(x, y, 1)
+color = raytrace(E, (Q - E).norm(), scene)
+print "Took", time.time() - t0
+
+rgb = [Image.fromarray((255 * np.clip(c, 0, 1).reshape((h, w))).astype(np.uint8), "L") for c in color.components()]
+Image.merge("RGB", rgb).save("fig.png")
